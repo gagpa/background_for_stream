5ef10e5f1a372391b8f95c028cf202ee4b1fbc5e
gagpa <speitshot@gmail.com>
2020-10-26 01:48:00 +0300

    Реализован AJAX запрос на сторинний сервис для получния информаци на табло.

---
+++ js/ticker_info.js
@@ -0,0 +1,31 @@
+let tickerTitleElem = document.querySelector('.ticker__title .marquee');
+let tickerDataElem = document.querySelector('.ticker__data .marquee');
+const request = new XMLHttpRequest();
+const url = 'http://api_riot.local/tft/match_per_stream/ofry';
+
+function insertTickerInfo() {
+    let response = request.responseText;
+    let responseJson = JSON.parse(response);
+
+    let tickerTitle = responseJson.data.tickerTitle;
+    let tickerData = responseJson.data.tickerData;
+    if (!tickerTitleElem.textContent){
+        console.log('1');
+        tickerTitleElem.textContent = tickerTitle;
+    }
+    tickerDataElem.textContent = tickerData.length ? tickerData : 'Первая игра';
+}
+function sendRequest() {
+    request.open('GET', url);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-url');
+    request.addEventListener("readystatechange", () => {
+        if (request.readyState === 4 && request.status === 200) {
+            insertTickerInfo();
+        }
+    });
+
+    request.send();
+}
+
+sendRequest()
+setInterval(sendRequest, 10000)
